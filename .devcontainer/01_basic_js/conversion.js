c4b74d57da2119dd63ec0802028a47e06cab5718
iam-siddmas <maschatak24@gmail.com>
2025-06-09 08:36:13 +0000

    data type

---
+++ .devcontainer/01_basic_js/conversion.js
@@ -0,0 +1,10 @@
+let score = null;
+console.log(typeof score)
+console.log(typeof (score))
+let valueInnumber=Number(score)
+console.log(typeof valueInnumber)
+console.log(valueInnumber)
+
+let islogged=5
+let booleanIn=Boolean(islogged)
+console.log(booleanIn)
