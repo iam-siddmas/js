80006f58feca8c6cf82f821cd284305eabd15d56
iam-siddmas <maschatak24@gmail.com>
2025-06-10 12:09:39 +0000

    data type compare

--- .devcontainer/01_basic_js/conversion.js
+++ .devcontainer/01_basic_js/conversion.js
@@ -2,9 +2,22 @@ let score = null;
 console.log(typeof score)
 console.log(typeof (score))
 let valueInnumber=Number(score)
-console.log(typeof valueInnumber)
-console.log(valueInnumber)
+//console.log(typeof valueInnumber)
+//console.log(valueInnumber)
 
-let islogged=5
-let booleanIn=Boolean(islogged)
-console.log(booleanIn)
+// let islogged=5
+// let booleanIn=Boolean(islogged)
+//console.log(booleanIn)
+let value=3
+// console.log(1+5+"2"+"5");
+// console.log(+true);
+let gamechanger=10
+console.log(gamechanger);
+++gamechanger
+console.log(gamechanger);
+gamechanger++
+console.log(gamechanger);
+
+
+
+
